c25692a41644cadcf0b00eb24104ce63daa50206
maxxsyntax <root@amd64-livebld>
2023-10-25 17:59:12 -0600

    including jq queries

---
+++ burst.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+jq -r '.[] |  select( .tag=="wifi.client.probe") | select( .data.vendor | . == "") | .time'  | cut -d\. -f1 | sort | uniq -c | sort -nr | awk '{print "{\"quantity\": \"" $1 "\", \"date\": \"" $2 "\"}"}' | jq -s . | jq '.[] | select(.quantity > "1")' | jq -r .date
